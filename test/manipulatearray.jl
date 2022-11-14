384fdc30adc3bd916dee486430da8e29be80c599
okatsn <okatsn@gmail.com>
2022-11-14 11:52:19 +0800

    Test: reducetype

--- test/manipulatearray.jl
+++ test/manipulatearray.jl
@@ -32,3 +32,24 @@ using Dates
     @test isnothing(ifstrparse(nothing, Float64))
     @test isnan(ifstrparse(NaN, Float64))
 end
+
+@testset "reducetype" begin
+    v = [1,2,2.0,"str"] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Float64, Int64, String})
+
+    v = [2.0,"str"] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Float64, String})
+
+    v = ["str", missing] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Missing, String})
+
+    v = ["str", missing, NaN] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Missing, String, Float64})
+
+    v = ["str", 3.2, missing, NaN] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Missing, String, Float64})
+
+    v = [9.7, 3.2, missing, NaN] # a vector of any
+    @test isequal(eltype(reducetype(v)), Union{Missing, Float64})
+
+end
